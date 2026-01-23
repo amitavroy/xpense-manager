d6ff4800358cb4657724fc8a638dd2e868b56d2c
Amitav Roy <reachme@amitavroy.com>
2026-01-23 15:19:54 +0530

    Building features

--- public/build/assets/chevron-down-icbO5SDc.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./app-77O1mCLX.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],r=o("ChevronDown",n);export{r as C};
---
+++ public/build/assets/register-BvpWQZs0.js
@@ -0,0 +1,1 @@
+import{j as t,H as e}from"./app-BuFDx-Qe.js";import{A as r}from"./auth-layout-CWu13DTI.js";/* empty css            */import"./app-logo-icon-DNGdMlik.js";import"./index-CGZUerCI.js";function n(){return t.jsx(r,{title:"Create an account",description:"Enter your details below to create your account",children:t.jsx(e,{title:"Register"})})}export{n as default};

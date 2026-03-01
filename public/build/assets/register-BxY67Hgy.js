bcec1e6155e0379be6adc17086c2e90f805efcd3
Amitav Roy <reachme@amitavroy.com>
2026-03-01 12:53:26 +0530

    chore: added build

---
+++ public/build/assets/register-BxY67Hgy.js
@@ -0,0 +1,1 @@
+import{j as t,H as e}from"./app-BczfSJCN.js";import{A as r}from"./auth-layout-4lLmdy59.js";/* empty css            */import"./app-logo-icon-BhkbTNlu.js";import"./index-CGZUerCI.js";function n(){return t.jsx(r,{title:"Create an account",description:"Enter your details below to create your account",children:t.jsx(e,{title:"Register"})})}export{n as default};

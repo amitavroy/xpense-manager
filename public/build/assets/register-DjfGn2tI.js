f28ccf9d70239f3cffe1c73e5cba7b70e88723d5
Amitav Roy <reachme@amitavroy.com>
2026-08-22 10:10:45 +0530

    chore: updates

---
+++ public/build/assets/register-DjfGn2tI.js
@@ -0,0 +1,1 @@
+import{j as t,H as e}from"./app-Cy8KY4gD.js";import{A as r}from"./auth-layout-eGofD7fG.js";/* empty css            */import"./app-logo-icon-BJi4XkET.js";import"./index-BfpPYucJ.js";function n(){return t.jsx(r,{title:"Create an account",description:"Enter your details below to create your account",children:t.jsx(e,{title:"Register"})})}export{n as default};
